8af12cf96bc3ab411636cfd45e087795f446f73a
loooh.hoy6@gmail.com <loooh.hoy6@gmail.com>
2026-08-20 11:38:45 +0300

    feat(program): configure exception middleware, sqlite db, and static files

--- src/WalletService.WebAPI/Program.cs
+++ src/WalletService.WebAPI/Program.cs
@@ -1,7 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 using WalletService.Infrastructure.Persistence;
+using WalletService.Application.Abstractions;
+using WalletService.Application.Services;
+using WalletService.WebAPI.Middlewares;
+
+
+
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+
 
 
 builder.Services.AddControllers(); 
@@ -12,7 +22,15 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
+
+builder.Services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+builder.Services.AddScoped<IWalletService, WalletService.Application.Services.WalletService>();
+
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 
 if (app.Environment.IsDevelopment())
@@ -20,6 +38,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+
+app.UseStaticFiles();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 
